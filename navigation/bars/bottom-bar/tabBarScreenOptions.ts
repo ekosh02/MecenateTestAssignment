3d1d0cf32762c1f0eb389249c33e9aa5938bedd9
Yeldos Turapbayev <101699011+ekosh02@users.noreply.github.com>
2026-04-14 03:47:43 +0500

    fixed bottom bar focus color

--- navigation/bars/bottom-bar/tabBarScreenOptions.ts
+++ navigation/bars/bottom-bar/tabBarScreenOptions.ts
@@ -1,8 +1,9 @@
+import { COLORS } from "@/constants/colors";
 import type { BottomTabNavigationOptions } from "@react-navigation/bottom-tabs";
 
 export const bottomTabScreenOptions: BottomTabNavigationOptions = {
   headerShown: false,
-  tabBarActiveTintColor: "#111827",
+  tabBarActiveTintColor: COLORS.PRIMARY,
   tabBarInactiveTintColor: "#8C95A8",
   tabBarStyle: {
     backgroundColor: "#FFFFFF",
